45ebb9dc7615046d6a896d27c9df56785cbb293e
chaithra505 <kotianchaithra74@gmail.com>
2021-07-23 21:54:26 +0530

    Text to speech Application

---
+++ app/src/main/java/com/example/a4su18is006app7/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.a4su18is006app7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    TextToSpeech t1;
+    EditText txtinput;
+    Button txt2speech;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtinput = findViewById(R.id.txt_input);
+        txt2speech = findViewById(R.id.btn_txt2spch);
+
+        t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.UK);
+                }
+            }
+        });
+
+        txt2speech.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tospeak = txtinput.getText().toString();
+                Toast.makeText(getBaseContext(),tospeak, Toast.LENGTH_SHORT).show();
+
+                t1.speak(tospeak,TextToSpeech.QUEUE_FLUSH,null);
+            }
+        });
+    }
+
+    public void onPause()
+    {
+        if(t1 != null)
+        {
+            t1.stop();
+            t1.shutdown();
+        }
+        super.onPause();
+    }
+
+}
